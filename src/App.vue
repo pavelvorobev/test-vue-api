bf8216bae583fef904e3caca1855c8cfafff0205
Pavel <pavel_9_5@mail.ru>
2021-09-25 19:23:49 +0300

    Добавляет получение данных с сервера

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav>
+    <div class="nav-wrapper">
+      <ul class="left hide-on-med-and-down">
+        <li> <a class='dropdown-button' href='#' data-activates='dropdown1' data-beloworigin="true">Menu1</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <ul id='dropdown1' class='dropdown-content dropdown-nested'>
+    <li><a class='dropdown-button' href='#!' data-activates='dropdown2' data-hover="true" data-alignment="left">1<span class="right-triangle">&#9656;</span></a></li>
+    <li><a href="#!">2</a></li>
+    <li><a href="#!">3</a></li>
+  </ul>
+
+  <ul id='dropdown2' class='dropdown-content dropdown-nested'>
+    <li><a href="#!">4</a></li>
+    <li><a class='dropdown-button' href="#!" data-activates="dropdown3" data-hover="hover" data-alignment="left">5<span class="right-triangle">&#9656;</span></a></li>
+    <li><a href="#!">6</a></li>
+  </ul>
+
+  <ul id='dropdown3' class='dropdown-content dropdown-nested'>
+    <li><a href="#!">7</a></li>
+    <li><a href="#!">8</a></li>
+    <li><a href="#!">9</a></li>
+    <li><a href="#!">10</a></li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* import M from 'materialize-css'; */
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  /* fetch('https://belferat.online/test.php ', {
+    method: 'POST',
+  }) */
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .right-triangle {
+    float: right;
+  }
+
+  .dropdown-nested {
+    width: max-content !important;
+    max-width: 300px !important;
+    overflow-y: visible !important;
+  }
+
+  .dropdown-content .dropdown-content {
+    margin-left: 100%;
+  }
 </style>
